6752af26731161909775bfd24cb9051ddc119030
samarth-web <samarthj204@gmail.com>
2025-01-09 19:40:57 +0000

    Playlist Creation

--- app.py
+++ app.py
@@ -2,18 +2,33 @@ import spotipy
 from dotenv import load_dotenv
 import os
 from spotipy.oauth2 import SpotifyOAuth
+import time
 load_dotenv(dotenv_path="../.env")
 CLIENT_ID = os.getenv('SPOTIPY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIPY_CLIENT_SECRET')
 REDIRECT_URI = os.getenv('SPOTIPY_REDIRECT_URI')
-scope = "user-top-read"
+scope = "user-top-read playlist-modify-public"
 authorizer = SpotifyOAuth( client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope)
 music = spotipy.Spotify(auth_manager=authorizer)
-top_tracks = music.current_user_top_tracks(limit=20, offset=0, time_range='long_term')
+time_pref = 'long_term'
+top_tracks = music.current_user_top_tracks(limit=50, offset=0, time_range=time_pref)
+song_links = []
 counter = 1
 for track in top_tracks['items']:
     track_name = track['name']
     artists = ", ".join([artist['name'] for artist in track['artists']])
     print(f"{counter}. {track_name} by {artists}")
+    song_links.append(track['uri'])
     counter = counter + 1
+print("Public playlist created in Spotify account")
+user_id = music.current_user()['id']
+playlist_name = "My Top 50 Tracks"
+playlist_description = f"A playlist of my {time_pref} top 50 tracks via Spotify API"
+playlist_created = music.user_playlist_create(     user=user_id,      name=playlist_name,     public=True,      description=playlist_description )
+time.sleep(2)
+music.playlist_add_items(playlist_id=playlist_created['id'], items=song_links)
+
+
+
+
 
