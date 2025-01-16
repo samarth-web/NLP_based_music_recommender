35ad6817f2adbea6d826f2e846ba932c3477e322
samarth-web <samarthj204@gmail.com>
2025-01-16 21:55:08 +0000

    Added more changes to emotion detection

--- app.py
+++ app.py
@@ -13,31 +13,32 @@ def emotion_detection():
     print(ret)
 
             
-    enforce_detection=False    
+      
             
 
     cam.release()
     if ret == False:
         print("No camera detected")
     else:
+        enforce_detection=False  
         print("Camera detected")
-        analysis = DeepFace.analyze(frame, actions=['race', 'emotion', 'age', 'gender'])
-        if isinstance(analysis, list):
+        analysis = DeepFace.analyze(frame, actions=['emotion'])
+        
+        print("analysis is a list")
+        
 
-            print("analysis is a list")
-        else:
-
-            print("analysis is not a list")
-            print(analysis)
-            print(len(analysis))
-            trial = analysis[0]
-            print("Race analysis:", trial.get("dominant_race"))
-            print("Emotion:", trial.get("dominant_emotion"))
-            print("Age:", trial.get("age"))
-            print("Gender:", trial.get("dominant_gender"))
+            # print("analysis is not a list")
+            # print(analysis)
+            # print(len(analysis))
+        trial = analysis[0]
+            # print("Race analysis:", trial.get("dominant_race"))
+            # print("Emotion:", trial.get("dominant_emotion"))
+            # print("Age:", trial.get("age"))
+            # print("Gender:", trial.get("dominant_gender"))
             # print("Age:", analysis.get("age"))
-            # print("Gender:", analysis.get("gender"))
-            return trial.get("dominant_emotion")
+            # print("Gender:", analysis.get("gender")
+        print(trial.get("dominant_emotion"))
+        return trial.get("dominant_emotion")
 
 def music_creation():
     load_dotenv(dotenv_path="../.env")
@@ -52,34 +53,57 @@ def music_creation():
     song_links = []
     counter = 1
 
-    var = 0
-    for track in top_tracks['items']:
-        track_name = track['name']
-        artists = ", ".join([artist['name'] for artist in track['artists']])
-        print(f"{counter}. {track_name} by {artists}")
-        song_links.append(track['uri'])
-        counter = counter + 1
-    print("Public playlist created in Spotify account")
-    user_id = music.current_user()['id']
-    playlist_name = "My Top 50 Tracks"
-    playlist_description = f"A playlist of my {time_pref} top 50 tracks via Spotify API"
-    playlist_created = music.user_playlist_create( user=user_id,  name=playlist_name,  public=True,  description=playlist_description )
-    time.sleep(2)
-    music.playlist_add_items(playlist_id=playlist_created['id'], items=song_links)
-    print("Check your spotify app now : ")
-    playlist_id = playlist_created['id']
-    embed_url = f"https://open.spotify.com/embed/playlist/{playlist_id}"
+    mood_params = {
+    "happy":   {"target_valence": 0.8, "target_energy": 0.7},
+    "sad":     {"target_valence": 0.2, "target_energy": 0.3},
+    "angry":   {"target_valence": 0.1, "target_energy": 0.9},
+    "neutral": {"target_valence": 0.5, "target_energy": 0.5},
+    "disgust": {"target_valence": 0.1, "target_energy": 0.5},
+}
+    emotion = emotion_detection()
 
-    st.title('Mood Music Recommendation')
-    st.markdown(
-        f"""
-        <iframe src="{embed_url}" 
-        width="400" 
-        height="600" 
-        frameborder="0" 
-        allowtransparency="true" 
-        allow="encrypted-media"></iframe> 
-        """,
-        unsafe_allow_html=True #tested
+    recs = music.recommendations(
+        seed_genres=["pop"], 
+        limit=10,
+        target_valence=mood_params[emotion]["target_valence"],
+        target_energy=mood_params[emotion]["target_energy"]
     )
-     img_data = st.camera_input("Take a photo")
+    user_id = music.current_user()['id']
+    playlist_name = "Your Mix using face detection"
+    playlist_description = "Created using Machine learning and face detection (Deep Face)"
+    playlist_created = music.user_playlist_create( user=user_id,  name=playlist_name,  public=True,  description=playlist_description )
+    song_links = [track["uri"] for track in recs["tracks"]]
+    music.playlist_add_items(playlist_id=playlist_created['id'], items=song_links)
+    # var = 0
+    # for track in top_tracks['items']:
+    #     track_name = track['name']
+    #     artists = ", ".join([artist['name'] for artist in track['artists']])
+    #     print(f"{counter}. {track_name} by {artists}")
+    #     song_links.append(track['uri'])
+    #     counter = counter + 1
+    # print("Public playlist created in Spotify account")
+    # user_id = music.current_user()['id']
+    # playlist_name = "My Top 50 Tracks"
+    # playlist_description = f"A playlist of my {time_pref} top 50 tracks via Spotify API"
+    # playlist_created = music.user_playlist_create( user=user_id,  name=playlist_name,  public=True,  description=playlist_description )
+    # time.sleep(2)
+    # music.playlist_add_items(playlist_id=playlist_created['id'], items=song_links)
+    # print("Check your spotify app now : ")
+    # playlist_id = playlist_created['id']
+    # embed_url = f"https://open.spotify.com/embed/playlist/{playlist_id}"
+
+    # st.title('Mood Music Recommendation')
+    # st.markdown(
+    #     f"""
+    #     <iframe src="{embed_url}" 
+    #     width="400" 
+    #     height="600" 
+    #     frameborder="0" 
+    #     allowtransparency="true" 
+    #     allow="encrypted-media"></iframe> 
+    #     """,
+    #     unsafe_allow_html=True #tested
+    #)
+if __name__ == "__main__":
+    music_creation()
+    
